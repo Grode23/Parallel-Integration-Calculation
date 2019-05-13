78b447e6bf5446624b46da78b0b4f07c2da75947
grode <dimitris23bp@gmail.com>
2019-05-14 00:33:28 +0300

    Progress with thread pool

--- src/NumIntSeq.java
+++ src/NumIntSeq.java
@@ -1,37 +1,73 @@
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class NumIntSeq {
 
-    public static void main(String[] args) {
+	final static int CORES = Runtime.getRuntime().availableProcessors();	
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException{
 
-        long numSteps = 0;
-        double sum = 0.0;
+		long numSteps = 0;
+		double sum = 0.0;
+		int tasks = 20;
 
-        /* parse command line */
-        if (args.length != 1) {
-		System.out.println("arguments:  number_of_steps");
-                System.exit(1);
-        }
-        try {
-		numSteps = Long.parseLong(args[0]);
-        } catch (NumberFormatException e) {
-		System.out.println("argument "+ args[0] +" must be long int");
-		System.exit(1);
-        }
-        /* start timing */
-        long startTime = System.currentTimeMillis();
 
-        double step = 1.0 / (double)numSteps;
-        /* do computation */
-        for (long i=0; i < numSteps; ++i) {
-            double x = ((double)i+0.5)*step;
-            sum += 4.0/(1.0+x*x);
-        }
-        double pi = sum * step;
+		/* parse command line */
+		if (args.length != 1) {
+			System.out.println("arguments:  number_of_steps");
+			System.exit(1);
+		}
+		
+		try {
+			numSteps = Long.parseLong(args[0]);
+		} catch (NumberFormatException e) {
+			System.out.println("argument " + args[0] + " must be long int");
+			System.exit(1);
+		}
+		
+		
+		ExecutorService pool = Executors.newFixedThreadPool(CORES);
+		ArrayList<Future<Double>> futures = new ArrayList<Future<Double>>();
+		
+		
+		
+		/* start timing */
+		long startTime = System.currentTimeMillis();
 
-        /* end timing and print result */
-        long endTime = System.currentTimeMillis();
-        System.out.printf("sequential program results with %d steps\n", numSteps);
-        System.out.printf("computed pi = %22.20f\n" , pi);
-        System.out.printf("difference between estimated pi and Math.PI = %22.20f\n", Math.abs(pi - Math.PI));
-        System.out.printf("time to compute = %f seconds\n", (double) (endTime - startTime) / 1000);
-    }
+		double step = 1.0 / (double) numSteps;
+		Future<Double> result;
+		
+		//Loops the task is going to take care of
+		long loops = numSteps / CORES;
+		//First and last values of loops per task
+		long start = 0, finish = 0;
+		
+		for(int i = 0; i < tasks; i++) {
+			
+			finish += loops;
+			result = pool.submit(new ThreadStuff(step, start, finish));
+			start += loops;
+			
+			futures.add(result);
+			
+			sum += futures.get(i).get();
+			System.out.println(sum);
+		}
+
+		
+		double pi = sum * step;
+
+		/* end timing and print result */
+		long endTime = System.currentTimeMillis();
+		
+		pool.shutdown();
+		
+		System.out.printf("sequential program results with %d steps\n", numSteps);
+		System.out.printf("computed pi = %22.20f\n", pi);
+		System.out.printf("difference between estimated pi and Math.PI = %22.20f\n", Math.abs(pi - Math.PI));
+		System.out.printf("time to compute = %f seconds\n", (double) (endTime - startTime) / 1000);
+	}
 }
---
+++ src/ThreadStuff.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.Callable;
+
+class ThreadStuff implements Callable<Double>{
+
+	private long start, finish;
+	private double step;
+	private double sum = 0.0;
+
+	
+	public ThreadStuff(double step, long start, long finish) {
+		this.step = step;
+		this.start = start;
+		this.finish = finish;
+	}
+	
+
+	@Override
+	public Double call() throws Exception {
+
+		/* do computation */
+		for (long i = start; i < finish; ++i) {
+			double x = ((double) i + 0.5) * step;
+			sum += 4.0 / (1.0 + x * x);
+		}
+		
+		return sum;
+	}
+	
+	
+	
+	
+}
