c87ebc94892574891cff8c546e0eea579bfb9d6f
grode <dimitris23bp@gmail.com>
2019-05-23 21:22:49 +0300

    Preface of ForkJoin

--- src/NumIntSeq.java
+++ src/NumIntSeq.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,12 +12,11 @@ public class NumIntSeq {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 
 		long numSteps = 0;
-		double sum = 0.0;
+		double pi = 0.0;
 
 		/* parse command line */
-		if (args.length != 1 && args.length != 2) {
+		if (args.length != 1) {
 			System.out.println("args[0]:  number_of_steps");
-			System.out.println("args[1]:  number_of_tasks (default == 20)");
 			System.exit(1);
 		}
 
@@ -27,20 +27,46 @@ public class NumIntSeq {
 			System.exit(1);
 		}
 
-		// Number of tasks by default
-		int tasks = 5;
+		// Choose between the available options
+		Scanner sc = new Scanner(System.in);
+		int decision;
 
-		// Number of tasks, if it's not the default one
-		if (args.length == 2) {
+		do {
+			System.out.println("Choose option: (1 or 2)");
+			System.out.println("1. ThreadPool");
+			System.out.println("2. ForkJoin");
 
-			try {
-				tasks = Integer.parseInt(args[1]);
-			} catch (NumberFormatException e) {
-				System.out.println("argument " + args[1] + " must be int");
-				System.exit(1);
-			}
+			decision = sc.nextInt();
 
+		} while (decision != 1 && decision != 2);
+
+		/* start timing */
+		long startTime = System.currentTimeMillis();
+
+		switch (decision) {
+		case 1:
+			pi = runThreadPoolOption(numSteps);
+		case 2:
+			pi = runForkJoinOption(numSteps);
+		default:
+			break;
 		}
+
+		/* end timing and print result */
+		long endTime = System.currentTimeMillis();
+
+		sc.close();
+
+		System.out.printf("sequential program results with %d steps\n", numSteps);
+		System.out.printf("computed pi = %22.20f\n", pi);
+		System.out.printf("difference between estimated pi and Math.PI = %.20f\n", Math.abs(pi - Math.PI));
+		System.out.printf("time to compute = %f seconds\n", (double) (endTime - startTime) / 1000);
+	}
+
+	private static double runThreadPoolOption(long numSteps) throws InterruptedException, ExecutionException {
+
+		double sum = 0.0;
+		int tasks = 20; // Number of tasks by default
 
 		// Pool with as much threads as the number of available cores
 		ExecutorService pool = Executors.newFixedThreadPool(CORES);
@@ -51,9 +77,6 @@ public class NumIntSeq {
 		long loops = numSteps / tasks;
 		// First and last values of loops per task
 		long start = 0, finish = 0;
-
-		/* start timing */
-		long startTime = System.currentTimeMillis();
 
 		double step = 1.0 / (double) numSteps;
 		for (int i = 0; i < tasks; i++) {
@@ -68,15 +91,17 @@ public class NumIntSeq {
 		// My pi
 		double pi = sum * step;
 
-		/* end timing and print result */
-		long endTime = System.currentTimeMillis();
-
 		// Kill the threads
 		pool.shutdown();
 
-		System.out.printf("sequential program results with %d steps\n", numSteps);
-		System.out.printf("computed pi = %22.20f\n", pi);
-		System.out.printf("difference between estimated pi and Math.PI = %.20f\n", Math.abs(pi - Math.PI));
-		System.out.printf("time to compute = %f seconds\n", (double) (endTime - startTime) / 1000);
+		return pi;
+
+	}
+	
+	
+	@SuppressWarnings("null")
+	private static double runForkJoinOption(long numSteps) {
+		
+		return (Double) null;
 	}
 }
